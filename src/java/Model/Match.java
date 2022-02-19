f9c9b61f2b681c83299c10613760042dfb738db5
hoan111 <hoan489@gmail.com>
2022-02-20 00:12:26 +0700

    Update match class

--- src/java/Model/Match.java
+++ src/java/Model/Match.java
@@ -19,5 +19,61 @@ public class Match {
     private String type;
     private int playedGame;
     private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getServerID() {
+        return serverID;
+    }
+
+    public void setServerID(int serverID) {
+        this.serverID = serverID;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPlayedGame() {
+        return playedGame;
+    }
+
+    public void setPlayedGame(int playedGame) {
+        this.playedGame = playedGame;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
     
 }
