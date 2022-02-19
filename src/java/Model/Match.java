2f1d746efc8b9279b1063e0e55190db21a1c88fa
hoan111 <hoan489@gmail.com>
2022-02-19 23:58:21 +0700

    init project

---
+++ src/java/Model/Match.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hoan
+ */
+public class Match {
+    private int id;
+    private int serverID;
+    private Date startTime;
+    private Date endTime;
+    private String type;
+    private int playedGame;
+    private double price;
+    
+}
---
+++ src/java/Model/Server.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hoan
+ */
+public class Server {
+    private String id;
+    private String ip;
+    private String port;
+    private String serverPassword;
+    private String rconPassword;
+    private Date createdDate;
+    private boolean isActive;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getServerPassword() {
+        return serverPassword;
+    }
+
+    public void setServerPassword(String serverPassword) {
+        this.serverPassword = serverPassword;
+    }
+
+    public String getRconPassword() {
+        return rconPassword;
+    }
+
+    public void setRconPassword(String rconPassword) {
+        this.rconPassword = rconPassword;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+}
