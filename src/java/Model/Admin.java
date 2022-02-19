43436d0bc77f966f05b87451c1dbedbd21078e09
hoan111 <hoan489@gmail.com>
2022-02-20 00:12:40 +0700

    Update admin class

--- src/java/Model/Admin.java
+++ src/java/Model/Admin.java
@@ -10,5 +10,41 @@ package Model;
  * @author hoan
  */
 public class Admin {
+    private int id;
+    private String username;
+    private String password;
+    private String apikey;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
     
 }
